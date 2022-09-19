348dd757c7d7f34a020088291e611160df2aedf4
Livuza <livuzacharles@gmail.com>
2022-09-19 21:53:05 +0300

    mall customer segmentation requirements

--- mall_customer_segmentation.py
+++ mall_customer_segmentation.py
@@ -1,7 +1,7 @@
 from email.policy import default
 import pandas as pd
 import numpy as np
-# import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import streamlit as st
