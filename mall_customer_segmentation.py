aaa60171e4f68420980e4dda65760d6e30569f02
Livuza <livuzacharles@gmail.com>
2022-09-29 22:32:47 +0300

    background

--- mall_customer_segmentation.py
+++ mall_customer_segmentation.py
@@ -213,20 +213,20 @@ st.markdown("This model predicts the mall customer gender depending on age, annu
 # df
 
 
-import base64
+# import base64
 
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"jpeg"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('mall.jpeg')  
+# def add_bg_from_local(image_file):
+#     with open(image_file, "rb") as image_file:
+#         encoded_string = base64.b64encode(image_file.read())
+#     st.markdown(
+#     f"""
+#     <style>
+#     .stApp {{
+#         background-image: url(data:image/{"jpeg"};base64,{encoded_string.decode()});
+#         background-size: cover
+#     }}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+#     )
+# add_bg_from_local('mall.jpeg')  
